f667fd009a48f957528235e28559ea3021291665
jsil.tech@gmail.com <jsil.tech@gmail.com@7322292e-90b0-c5c8-8e06-53c4751e747b>
2012-07-25 04:18:50 +0000

    random nivel random palavras do nivel insert background

--- src/br/com/twolions/screen/QuestScreen.java
+++ src/br/com/twolions/screen/QuestScreen.java
@@ -1,10 +1,15 @@
 package br.com.twolions.screen;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import br.com.twolions.R;
@@ -16,9 +21,11 @@ import br.com.twolions.util.OnReturnAlertListener;
 public class QuestScreen extends ActivityCircle implements
 		OnReturnAlertListener {
 
+	private final String TAG = getClassName();
+
 	private ListaQuest lq;
 
-	Alert alert;
+	private Alert alert;
 
 	private int nivel;
 
@@ -53,13 +60,48 @@ public class QuestScreen extends ActivityCircle implements
 		TextView fig2 = (TextView) findViewById(R.id.figura2);
 		TextView fig3 = (TextView) findViewById(R.id.figura3);
 
+		// sorteio do nivel
+		Random generator = new Random();
+		int nivelDaVez = generator.nextInt(lq.FIGURAS.length);
+
+		// list of quest word
+		String[] wordQuests = lq.FIGURAS[nivelDaVez];
+
+		// embaralha ordem das palavras
+		ArrayList<String> fileList = new ArrayList<String>(wordQuests.length);
+
+		for (int i = 0; i < wordQuests.length; i++) {
+
+			Log.i(TAG, "Inserindo a palavra (" + wordQuests[i]
+					+ ") na fileList");
+
+			fileList.add(i, wordQuests[i]);
+		}
+
+		Log.i(TAG, "Embaralhando o fileList");
+		Collections.shuffle(fileList);
+
+		// array embaralhado
+		String[] listQuests = new String[fileList.size()];
+
+		for (int i = 0; i < fileList.size(); i++) {
+
+			Log.i(TAG, "Inserindo a palavra (" + fileList.get(i)
+					+ ") na listQuests");
+
+			listQuests[i] = fileList.get(i);
+
+		}
+
+		// String[] listQuests = Collections.shuffle(lq.FIGURAS[nivel]);
+
 		// settar figuras
 		try {
-			fig1.setText(lq.FIGURAS[nivel][0]);
+			fig1.setText(listQuests[0]);
 
-			fig2.setText(lq.FIGURAS[nivel][1]);
+			fig2.setText(listQuests[1]);
 
-			fig3.setText(lq.FIGURAS[nivel][2]);
+			fig3.setText(listQuests[2]);
 
 		} catch (ArrayIndexOutOfBoundsException e) {
 
@@ -73,8 +115,19 @@ public class QuestScreen extends ActivityCircle implements
 	}
 
 	// corrige nome do filme
+	TextView retorno;
+
+	ProgressBar progressBar;
+
 	public void verifyResponse(View v) {
 
+		// texto do centro
+		retorno = (TextView) findViewById(R.id.retorno);
+
+		// progesso central
+		progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+		// texto digitado pelo usuario
 		responseText = (EditText) findViewById(R.id.responseText);
 
 		String resposta = responseText.getText().toString().toLowerCase()
@@ -87,8 +140,28 @@ public class QuestScreen extends ActivityCircle implements
 			// resposta não preenchida
 			if (resposta.equals("")) {
 
-				Toast.makeText(this, "Por favor, responda algo.",
-						Toast.LENGTH_SHORT).show();
+				// some com a barra de progresso
+				progressBar.setVisibility(View.GONE);
+
+				// Toast.makeText(this,
+				// "Por favor, responda algo.",Toast.LENGTH_SHORT).show();
+
+				retorno.setText("Por favor, responda algo.");
+				retorno.setVisibility(View.VISIBLE);
+
+				Handler handler = new Handler();
+				handler.postDelayed(new Runnable() {
+					public void run() {
+						
+						// desaparece com a mensagem
+						retorno.setText("");
+						retorno.setVisibility(View.GONE);
+
+						// volta a exibir a barra de progresso
+						progressBar.setVisibility(View.VISIBLE);
+
+					}
+				}, 1500);
 
 				continue;
 			}
