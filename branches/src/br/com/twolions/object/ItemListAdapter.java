c3fbd08e68ef36b4e0a0374d83549c7192ba5ffb
jsil.tech@gmail.com <jsil.tech@gmail.com@7322292e-90b0-c5c8-8e06-53c4751e747b>
2012-09-13 03:44:42 +0000

    Branche 1

---
+++ branches/src/br/com/twolions/object/ItemListAdapter.java
@@ -0,0 +1,70 @@
+package br.com.twolions.object;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import br.com.twolions.R;
+import br.com.twolions.daoobjects.ItemLog;
+import br.com.twolions.daoobjects.Note;
+
+public class ItemListAdapter extends BaseAdapter {
+	private Context context;
+	private List<ItemLog> lista;
+
+	public ItemListAdapter(Context context, List<ItemLog> lista) {
+		this.context = context;
+		this.lista = lista;
+	}
+
+	public int getCount() {
+		return lista.size();
+	}
+
+	public Object getItem(int position) {
+		return lista.get(position);
+	}
+
+	public long getItemId(int position) {
+		return position;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// Recupera o Carro da posição atual
+		ItemLog item = lista.get(position);
+
+		LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View view = inflater.inflate(R.layout.item_log, null);
+
+		Note n;
+		// if (item.equals(n)) {
+
+		// }
+
+		// fuel
+
+		// expense
+
+		// note
+		TextView nome = (TextView) view.findViewById(R.id.nome);
+		// nome.setText(c.nome);
+
+		TextView placa = (TextView) view.findViewById(R.id.placa);
+		// placa.setText(c.placa);
+
+		ImageView tipo = (ImageView) view.findViewById(R.id.tipo);
+		/*
+		 * if (c.tipo.equals("carro")) {
+		 * tipo.setImageResource(R.drawable.type_car_on); } else {
+		 * tipo.setImageResource(R.drawable.type_moto_on); }
+		 */
+
+		return view;
+	}
+}
