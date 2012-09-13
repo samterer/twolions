cfc8ddb77b39a8172020a0d358030422aa20fff9
jsil.tech@gmail.com <jsil.tech@gmail.com@7322292e-90b0-c5c8-8e06-53c4751e747b>
2012-09-13 03:53:29 +0000

    Branche ItsMyCar

---
+++ branches/ItsMyCar/src/br/com/twolions/dao/ExpenseDAO.java
@@ -0,0 +1,154 @@
+package br.com.twolions.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+import br.com.twolions.daoobjects.Expense;
+import br.com.twolions.sql.DBConnection;
+
+public class ExpenseDAO extends DBConnection {
+	private static final String CATEGORIA = "base";
+
+	// Nome do banco
+	private static final String base_name = "db_itsmycar";
+	// Nome da tabela
+	public static final String table_name = "expense";
+
+	// private ManagerDAO dao;
+
+	public ExpenseDAO(Context ctx) {
+		super(ctx, base_name);
+		// dao = new ManagerDAO(ctx, base_name, table_name, db);
+	}
+
+	// Salva o carro, insere um novo ou atualiza
+	public long salvar(Expense expense) {
+		long id = expense.id;
+
+		if (id != 0) {
+			atualizar(expense);
+		} else {
+			// Insere novo
+			id = inserir(expense);
+		}
+
+		return id;
+	}
+
+	public long inserir(Expense expense) {
+		ContentValues values = new ContentValues();
+		values.put(Expense.ID_CAR, expense.id_car);
+		values.put(Expense.DATE, String.valueOf(expense.date));
+		values.put(Expense.SUBJECT, expense.subject);
+		values.put(Expense.VALUE, expense.value);
+		values.put(Expense.TIPO, expense.tipo);
+
+		long id = inserir(values, table_name);
+		return id;
+	}
+
+	public int atualizar(Expense expense) {
+		ContentValues values = new ContentValues();
+		values.put(Expense.ID_CAR, expense.id_car);
+		values.put(Expense.DATE, String.valueOf(expense.date));
+		values.put(Expense.SUBJECT, expense.subject);
+		values.put(Expense.VALUE, expense.value);
+		values.put(Expense.TIPO, expense.tipo);
+
+		String _id = String.valueOf(expense.id);
+
+		String where = Expense._ID + "=?";
+		String[] whereArgs = new String[]{_id};
+
+		int count = atualizar(values, where, whereArgs, table_name);
+
+		return count;
+	}
+
+	public int deletar(long id) {
+		String where = Expense._ID + "=?";
+
+		String _id = String.valueOf(id);
+		String[] whereArgs = new String[]{_id};
+
+		int count = deletar(where, whereArgs, table_name);
+
+		return count;
+	}
+
+	public Expense buscarExpense(long id) {
+		// select * from Expense where _id=?
+		Cursor c = db.query(true, table_name, Expense.colunas, Expense._ID
+				+ "=" + id, null, null, null, null, null);
+
+		if (c.getCount() > 0) {
+
+			// Posicinoa no primeiro elemento do cursor
+			c.moveToFirst();
+
+			Expense expense = new Expense();
+
+			// Lê os dados
+			expense.id = c.getLong(0);
+			expense.id_car = c.getLong(1);
+			expense.date = c.getString(2);
+			expense.subject = c.getString(3);
+			expense.value = c.getDouble(4);
+			expense.tipo = c.getString(5);
+
+			return expense;
+		}
+
+		return null;
+	}
+
+	// Retorna uma lista com todos os Expenses pelo id do carro
+	public List<Expense> listarExpenses(long id_Car) {
+		Cursor c = db.query(table_name, Expense.colunas, Expense.ID_CAR + "='"
+				+ id_Car + "'", null, null, null, null);
+
+		List<Expense> expenses = new ArrayList<Expense>();
+
+		if (c.moveToFirst()) {
+
+			// Recupera os índices das colunas
+			int idxId = c.getColumnIndex(Expense._ID);
+			int idxIdCar = c.getColumnIndex(Expense.ID_CAR);
+			int idxIdDate = c.getColumnIndex(Expense.DATE);
+			int idxIdSubject = c.getColumnIndex(Expense.SUBJECT);
+			int idxIdValue = c.getColumnIndex(Expense.VALUE);
+			int idxIdTipo = c.getColumnIndex(Expense.TIPO);
+
+			// Loop até o final
+			do {
+				Expense expense = new Expense();
+				expenses.add(expense);
+
+				// recupera os atributos de carro
+				expense.id = c.getLong(idxId);
+				expense.id_car = c.getLong(idxIdCar);
+				expense.date = c.getString(idxIdDate);
+				expense.subject = c.getString(idxIdSubject);
+				expense.value = c.getDouble(idxIdValue);
+				expense.tipo = c.getString(idxIdTipo);
+
+				Log.i(CATEGORIA, "Expense: " + expense.toString());
+
+			} while (c.moveToNext());
+		}
+
+		return expenses;
+	}
+
+	// Fecha o banco
+	public void fechar() {
+		// fecha o banco de dados
+		if (db != null) {
+			db.close();
+		}
+	}
+}
---
+++ branches/ItsMyCar/src/br/com/twolions/dao/FuelDAO.java
@@ -0,0 +1,154 @@
+package br.com.twolions.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+import br.com.twolions.daoobjects.Fuel;
+import br.com.twolions.sql.DBConnection;
+
+public class FuelDAO extends DBConnection {
+	private static final String CATEGORIA = "base";
+
+	// Nome do banco
+	private static final String base_name = "db_itsmycar";
+	// Nome da tabela
+	public static final String table_name = "fuel";
+
+	// private ManagerDAO dao;
+
+	public FuelDAO(Context ctx) {
+		super(ctx, base_name);
+		// dao = new ManagerDAO(ctx, base_name, table_name, db);
+	}
+
+	// Salva o carro, insere um novo ou atualiza
+	public long salvar(Fuel fuel) {
+		long id = fuel.id;
+
+		if (id != 0) {
+			atualizar(fuel);
+		} else {
+			// Insere novo
+			id = inserir(fuel);
+		}
+
+		return id;
+	}
+
+	public long inserir(Fuel fuel) {
+		ContentValues values = new ContentValues();
+		values.put(Fuel.ID_CAR, fuel.id_car);
+		values.put(Fuel.DATE, String.valueOf(fuel.date));
+		values.put(Fuel.VALUE_U, fuel.value_u);
+		values.put(Fuel.VALUE_P, fuel.value_p);
+		values.put(Fuel.ODOMETER, fuel.odometer);
+
+		long id = inserir(values, table_name);
+		return id;
+	}
+
+	public int atualizar(Fuel fuel) {
+		ContentValues values = new ContentValues();
+		values.put(Fuel.ID_CAR, fuel.id_car);
+		values.put(Fuel.DATE, String.valueOf(fuel.date));
+		values.put(Fuel.VALUE_U, fuel.value_u);
+		values.put(Fuel.VALUE_P, fuel.value_p);
+		values.put(Fuel.ODOMETER, fuel.odometer);
+
+		String _id = String.valueOf(fuel.id);
+
+		String where = Fuel._ID + "=?";
+		String[] whereArgs = new String[]{_id};
+
+		int count = atualizar(values, where, whereArgs, table_name);
+
+		return count;
+	}
+
+	public int deletar(long id) {
+		String where = Fuel._ID + "=?";
+
+		String _id = String.valueOf(id);
+		String[] whereArgs = new String[]{_id};
+
+		int count = deletar(where, whereArgs, table_name);
+
+		return count;
+	}
+
+	public Fuel buscarFuel(long id) {
+		// select * from Fuel where _id=?
+		Cursor c = db.query(true, table_name, Fuel.colunas,
+				Fuel._ID + "=" + id, null, null, null, null, null);
+
+		if (c.getCount() > 0) {
+
+			// Posicinoa no primeiro elemento do cursor
+			c.moveToFirst();
+
+			Fuel fuel = new Fuel();
+
+			// Lê os dados
+			fuel.id = c.getLong(0);
+			fuel.id_car = c.getLong(1);
+			fuel.date = c.getString(2);
+			fuel.value_u = c.getDouble(3);
+			fuel.value_p = c.getDouble(4);
+			fuel.odometer = c.getLong(5);
+
+			return fuel;
+		}
+
+		return null;
+	}
+
+	// Retorna uma lista com todos os Fuels pelo id do carro
+	public List<Fuel> listarFuels(long id_Car) {
+		Cursor c = db.query(table_name, Fuel.colunas, Fuel.ID_CAR + "='"
+				+ id_Car + "'", null, null, null, null);
+
+		List<Fuel> fuels = new ArrayList<Fuel>();
+
+		if (c.moveToFirst()) {
+
+			// Recupera os índices das colunas
+			int idxId = c.getColumnIndex(Fuel._ID);
+			int idxIdCar = c.getColumnIndex(Fuel.ID_CAR);
+			int idxIdDate = c.getColumnIndex(Fuel.DATE);
+			int idxIdValueU = c.getColumnIndex(Fuel.VALUE_U);
+			int idxIdValueP = c.getColumnIndex(Fuel.VALUE_P);
+			int idxIdOdometer = c.getColumnIndex(Fuel.ODOMETER);
+
+			// Loop até o final
+			do {
+				Fuel fuel = new Fuel();
+				fuels.add(fuel);
+
+				// recupera os atributos de carro
+				fuel.id = c.getLong(idxId);
+				fuel.id_car = c.getLong(idxIdCar);
+				fuel.date = c.getString(idxIdDate);
+				fuel.value_u = c.getDouble(idxIdValueU);
+				fuel.value_p = c.getDouble(idxIdValueP);
+				fuel.odometer = c.getLong(idxIdOdometer);
+
+				Log.i(CATEGORIA, "Expense: " + fuel.toString());
+
+			} while (c.moveToNext());
+		}
+
+		return fuels;
+	}
+
+	// Fecha o banco
+	public void fechar() {
+		// fecha o banco de dados
+		if (db != null) {
+			db.close();
+		}
+	}
+}
