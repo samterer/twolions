66b262e9ac7f938d704ab9fff23307cc2376051c
jsil.tech@gmail.com <jsil.tech@gmail.com@7322292e-90b0-c5c8-8e06-53c4751e747b>
2012-09-22 19:01:03 +0000

    não utilizo mais a biblioteca

--- Fuel/src/br/com/twolions/util/TimePickerFragment.java
+++ Fuel/src/br/com/twolions/util/TimePickerFragment.java
@@ -5,7 +5,6 @@ import java.util.Calendar;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
 import android.text.format.DateFormat;
 import android.widget.TimePicker;
 
@@ -13,7 +12,6 @@ public class TimePickerFragment extends DialogFragment
 		implements
 			TimePickerDialog.OnTimeSetListener {
 
-	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		// Use the current time as the default values for the picker
 		final Calendar c = Calendar.getInstance();
