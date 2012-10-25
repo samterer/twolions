d90ec425dda97dda588d57fc650b21b10616739f
jsil.tech@gmail.com <jsil.tech@gmail.com@7322292e-90b0-c5c8-8e06-53c4751e747b>
2012-10-25 00:39:47 +0000

    implement slide

---
+++ Fuel/src/br/com/twolions/model/Expense.java
@@ -0,0 +1,59 @@
+package br.com.twolions.model;
+
+import android.provider.BaseColumns;
+
+public class Expense implements BaseColumns {
+
+	public static String[] colunas = new String[]{Expense._ID, Expense.ID_CAR,
+			Expense.DATE, Expense.SUBJECT, Expense.TIPO};
+
+	/**
+	 * Pacote do Content Provider. Precisa ser único.
+	 */
+	public static final String AUTHORITY = "br.com.twolions";
+
+	public long id;
+	public long id_car;
+	public String date;
+	public String subject;
+	public double value;
+	public String tipo;
+
+	public static final String DEFAULT_SORT_ORDER = "_id ASC";
+
+	public static final String ID_CAR = "id_car";
+	public static final String DATE = "date";
+	public static final String SUBJECT = "subject";
+	public static final String VALUE = "value";
+	public static final String TIPO = "tipo";
+
+	public Expense() {
+	}
+
+	public Expense(final long id_car, final String date, final String subject,
+			final double value, final String tipo) {
+		super();
+		this.id_car = id_car;
+		this.date = date;
+		this.subject = subject;
+		this.value = value;
+		this.tipo = tipo;
+	}
+
+	public Expense(final long id, final long id_car, final String date,
+			final String subject, final double value, final String tipo) {
+		super();
+		this.id = id;
+		this.id_car = id_car;
+		this.date = date;
+		this.subject = subject;
+		this.value = value;
+		this.tipo = tipo;
+	}
+
+	@Override
+	public String toString() {
+		return "Date: " + date + ", Subject: " + subject + ", Value: " + value
+				+ ", Tipo: " + tipo;
+	}
+}
