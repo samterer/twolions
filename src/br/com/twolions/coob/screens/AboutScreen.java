54446ea5d3073d9a81e0e53536c9b7acd8db3ceb
jsil.tech@gmail.com <jsil.tech@gmail.com@7322292e-90b0-c5c8-8e06-53c4751e747b>
2012-11-13 02:22:46 +0000

    implement - correção tela de about - exclusão de métodos não utilizados - exclusão de classes não utilizadas

--- src/br/com/twolions/coob/screens/AboutScreen.java
+++ src/br/com/twolions/coob/screens/AboutScreen.java
@@ -1,22 +1,11 @@
 package br.com.twolions.coob.screens;
 
-import java.util.Vector;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import br.com.twolions.coob.R;
 import br.com.twolions.coob.core.ActivityCircle;
-import br.com.twolions.coob.util.HexValidator;
 
 public class AboutScreen extends ActivityCircle {
-
-	Vector<EditText> vEditText;
-
-	Vector<LinearLayout> vLinearLayout;
-
-	HexValidator hex;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -41,14 +30,14 @@ public class AboutScreen extends ActivityCircle {
 		overridePendingTransition(R.anim.scale_in, R.anim.scale_out);
 
 	}
-	
+
 	/****************************************************************
 	 * TOUCH
 	 ****************************************************************/
-	
+
 	public void hideAbout(View v) {
 		finish();
-		
+
 		overridePendingTransition(R.anim.scale_in, R.anim.scale_out);
 	}
 
