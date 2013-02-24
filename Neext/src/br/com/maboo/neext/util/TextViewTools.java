6bb902deb0516d1371e50ea4ba77a1d02cb1af0e
jsil.tech@gmail.com <jsil.tech@gmail.com@7322292e-90b0-c5c8-8e06-53c4751e747b>
2013-02-24 16:00:03 +0000

    implement - tentando implementa diferença de horas

--- Neext/src/br/com/maboo/neext/util/TextViewTools.java
+++ Neext/src/br/com/maboo/neext/util/TextViewTools.java
@@ -1,10 +1,15 @@
 package br.com.maboo.neext.util;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Vector;
 
 import android.content.Context;
 import android.graphics.Typeface;
-import android.text.format.Time;
+import android.net.ParseException;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,24 +26,39 @@ public class TextViewTools {
 	}
 
 	// retorna o tempo entre a data atual e a data passada
-	public static String getLastEdit(String hour, String date, Context context){
+	public static String getLastEdit(String hour, String date, Context context) {
 		String result = "";
-		
-		Time dt = new Time(); 
-		// campo 2 = /
-		// campo 5 = /
-		dt.set(Integer.valueOf(date.substring(0, 1)).intValue(), Integer
-				.valueOf(date.substring(3, 4)).intValue(),
-				Integer.valueOf(date.substring(6, 9)).intValue());
-		dt.normalize(true);
-		
-		dt.monthDay += 1;
-	
-		
-		Toast.makeText(context, hour+" "+dt, Toast.LENGTH_SHORT).show();
-		
-		
+
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Calendar cal = Calendar.getInstance();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+		try {
+			Date date1 = sdf.parse(date);
+			Date date2 = sdf.parse(dateFormat.format(cal.getTime()));
+			long differenceMilliSeconds = date2.getTime() - date1.getTime();
+			Log.i("appLog", "diferenca em milisegundos: "
+					+ differenceMilliSeconds);
+			Log.i("appLog", "diferenca em segundos: "
+					+ (differenceMilliSeconds / 1000));
+			Log.i("appLog", "diferenca em minutos: "
+					+ (differenceMilliSeconds / 1000 / 60));
+			Log.i("appLog", "diferenca em horas: "
+					+ (differenceMilliSeconds / 1000 / 60 / 60));
+			Log.i("appLog", "diferenca em dias: "
+					+ (differenceMilliSeconds / 1000 / 60 / 60 / 24));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} catch (java.text.ParseException e) {
+			e.printStackTrace();
+		}
+
+		Toast.makeText(
+				context,
+				"save date: '" + hour + " " + date + "' | current date: '"
+						+ dateFormat.format(cal.getTime()) + "' ",
+				Toast.LENGTH_LONG).show();
+
 		return result;
 	}
-
 }
