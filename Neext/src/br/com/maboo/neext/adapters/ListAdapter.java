92e0f7d0a8867809662ddd803010799eb8877204
jsil.tech@gmail.com <jsil.tech@gmail.com@7322292e-90b0-c5c8-8e06-53c4751e747b>
2013-03-26 04:07:05 +0000

    implement - layout for tablets - about direct to page in google play

--- Neext/src/br/com/maboo/neext/adapters/ListAdapter.java
+++ Neext/src/br/com/maboo/neext/adapters/ListAdapter.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
