a28a338f227e0db3d8434601ed84e55fc2aff76b
jsil.tech <jsil.tech@7322292e-90b0-c5c8-8e06-53c4751e747b>
2013-08-19 22:53:28 +0000

    > leitura dinamica do campo de subject na lista > correção do erro de tela com o milestone

---
+++ Neext/src/br/com/maboo/neext/util/TextViewSubject.java
@@ -0,0 +1,14 @@
+package br.com.maboo.neext.util;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class TextViewSubject extends TextView {
+
+	public TextViewSubject(Context context, AttributeSet attrs) {
+		super(context);
+
+	}
+
+}
