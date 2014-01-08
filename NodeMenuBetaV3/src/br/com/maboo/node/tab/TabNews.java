e44f0c67b6b724c0b6932398340285bb05f66f9c
jsil.tech <jsil.tech@7322292e-90b0-c5c8-8e06-53c4751e747b>
2014-01-08 17:23:15 +0000

    aba friends retirada aba news inserida bt friends inserido no menu lateral

---
+++ NodeMenuBetaV3/src/br/com/maboo/node/tab/TabNews.java
@@ -0,0 +1,21 @@
+package br.com.maboo.node.tab;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import br.com.maboo.node.R;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+public class TabNews extends SherlockFragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Get the view from fragmenttab2.xml
+		View view = inflater
+				.inflate(R.layout.fragmentprofile, container, false);
+
+		return view;
+	}
+}
