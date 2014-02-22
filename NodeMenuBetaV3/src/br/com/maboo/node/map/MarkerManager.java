c576571bf2d5764e1d26bfd335731295ef44e7c9
jsil.tech <jsil.tech@7322292e-90b0-c5c8-8e06-53c4751e747b>
2014-02-22 06:03:43 +0000

    animacao diferenciada nos bts da barra de ino

--- NodeMenuBetaV3/src/br/com/maboo/node/map/MarkerManager.java
+++ NodeMenuBetaV3/src/br/com/maboo/node/map/MarkerManager.java
@@ -1,26 +1,23 @@
 package br.com.maboo.node.map;
 
-import java.util.ArrayList;
 import java.util.Date;
 
 import android.app.Activity;
-import android.content.Context;
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import br.com.maboo.node.R;
 import br.com.maboo.node.chat.ChatActivity;
 import br.livroandroid.utils.AndroidUtils;
 
-import com.facebook.scrumptious.auxiliar.FaceUserVO;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * classe que controla os pontos no mapa
@@ -30,11 +27,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
  */
 public class MarkerManager {
 
-	private String TAG = "GeoPointManager";
+	private String TAG = "MarkerManager";
 
 	private GoogleMap map;
 	private Activity act;
 	private View view;
+
+	private Location loc;
 
 	// posicao inicial do mapa
 	public void initPointManager(final GoogleMap map,
@@ -42,13 +41,8 @@ public class MarkerManager {
 		this.map = map;
 		this.view = view;
 
-		final Location loc = map.getMyLocation();
-
-		// cria marcador do usuario (icone do user)
-
-		// map.addMarker(new MarkerOptions().position(
-		// new LatLng(loc.getLatitude(), loc.getLongitude())).title(
-		// "It's Me! " + FaceUserVO.user_name));
+		// localização do usuario
+		loc = map.getMyLocation();
 
 		map.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
 
@@ -109,58 +103,29 @@ public class MarkerManager {
 			}
 		});
 
-		// cria os nodes no mapa (será numa task separada)
-		// criaPonto();
-
-		// icone do usuario
-		// criaIconUser(loc);
-	}
-
-	private void criaIconUser(Location loc) {
-
-		map.addMarker(new MarkerOptions()
-				.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_mark))
-				.title(FaceUserVO.user_name).snippet("estou...")
-				.anchor(0.0f, 1.0f)
-				.position(new LatLng(loc.getLatitude(), loc.getLongitude()))
-				.flat(true));
+		// listener dos bts da barra da informação
+		ImageView btPrivate = (ImageView) view.findViewById(R.id.btPrivate);
+		btPrivate.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				criaNodePrivado(v);
+			}
+		});
+		ImageView btPublic = (ImageView) view.findViewById(R.id.btPublic);
+		btPublic.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				criaNodePublico(v);
+			}
+		});
 	}
 
 	/*******************************************************************************
-	 * Cria os pontos
+	 * seleciona um dos chats
 	 *******************************************************************************/
-	private void criaPonto() {
-
-		// Toast.makeText(act.getApplicationContext(), "criaPonto...",
-		// Toast.LENGTH_SHORT).show();
-
-		// recupera a lista de pontos
-		ArrayList<MarkerVO> list = new ListaMarker().getList();
-
-		for (int i = 0; i < list.size(); i++) {
-
-			MarkerVO g = list.get(i);
-
-			Log.i(TAG,
-					"criando ponto...: " + g.getDesc() + " | lat,Lon:"
-							+ g.getLatLng().latitude + ","
-							+ g.getLatLng().longitude);
-
-			map.addMarker(new MarkerOptions()
-					.icon(BitmapDescriptorFactory
-							.fromResource(R.drawable.ic_mark))
-					.title(g.getDesc()).snippet("teste...").anchor(0.0f, 1.0f)
-					.position(g.getLatLng()).flat(true));
-		}
-
+	public void criaNodePublico(View v) {
+		Log.i(TAG, "cria node publico...");
 	}
 
-	public static int convertToPixels(Context context, int nDP) {
-		final float conversionScale = context.getResources()
-				.getDisplayMetrics().density;
-
-		return (int) ((nDP * conversionScale) + 0.5f);
-
+	public void criaNodePrivado(View v) {
+		Log.i(TAG, "cria node privado...");
 	}
-
 }
